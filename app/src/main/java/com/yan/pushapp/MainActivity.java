2fa914badada3573f8b8871998e69e20b1e5d1b5
yanci <494742298@qq.com>
2019-03-07 17:24:40 +0800

    1

--- app/src/main/java/com/yan/pushapp/MainActivity.java
+++ app/src/main/java/com/yan/pushapp/MainActivity.java
@@ -5,18 +5,32 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ConnectionFactory factory = new ConnectionFactory();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        startListen();
     }
 
     public void Push(View view){
@@ -45,12 +59,39 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     try {
                         Thread.sleep(5000);
-                        mNotificationManager.cancel("closed",1);
+                        mNotificationManager.cancel("closed", 1);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }).start();
         }
+    }
+    public void init(){
+        factory.setHost("192.168.0.102");// MQ的IP
+        factory.setPort(5672);// MQ端口
+        factory.setUsername("root");// MQ用户名
+        factory.setPassword("root");// MQ密码
+    }
+    public void startListen(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Connection connection = factory.newConnection();
+                    Channel channel = connection.createChannel();
+                    channel.queueDeclare("GMessage", true, false, false, null);
+                    Consumer consumer = new DefaultConsumer(channel){
+                        @Override
+                        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                            System.out.println(new String(body));
+                        }
+                    };
+                    channel.basicConsume("GMessage", true, consumer);
+                } catch (IOException | TimeoutException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
